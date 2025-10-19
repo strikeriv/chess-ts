f06a1f99dcfe34d04e2107330347c085b8c4e86d
= <=>
2025-10-19 18:37:07 -0500

    add knight moves

--- src/app/features/board/piece/services/piece-moves/knight.service.ts
+++ src/app/features/board/piece/services/piece-moves/knight.service.ts
@@ -1,48 +1,76 @@
 import { Injectable } from '@angular/core';
-import { NotationService } from '../../../services/notation/notation.service';
-import { MovingPiece, IntermediaryMove, MoveType } from '../interfaces/moves.interface';
+import { IntermediaryMove, MoveType, MovingPiece } from '../interfaces/moves.interface';
 import { SharedService } from './shared.service';
 
 @Injectable()
 export class KnightService {
-  constructor(
-    private readonly notationService: NotationService,
-    private readonly sharedServive: SharedService,
-  ) {}
+  constructor(private readonly sharedServive: SharedService) {}
 
   calculateMoves(piece: MovingPiece): IntermediaryMove[] {
     const { direction, square } = piece;
-    const { x } = this.notationService.chessToArrayNotation(square);
 
     // array for valid moves
     const moves: IntermediaryMove[] = [
+      // forward moves
       {
         notation: {
           x: direction * 2,
           y: direction * 1,
         },
         type: MoveType.MOVE,
-      }, // forward left
+      }, // far forward left
       {
         notation: {
           x: direction * 2,
           y: direction * -1,
         },
         type: MoveType.MOVE,
-      }, // forward right
-    ];
-
-    // check to see if we are doing our first move. if so, allow  two spaces forward
-    // only allowed on starting squares
-    if (x === 1 || x === 6) {
-      const localNotation = this.sharedServive.localMoveToAbsoluteMove(square, moves[0])!; // will always be valid
-
-      moves.push({
-        notation: { x: direction * 2, y: 0 },
+      }, // far forward right
+      {
+        notation: {
+          x: direction * 1,
+          y: direction * 2,
+        },
         type: MoveType.MOVE,
-        predecessor: this.notationService.arrayToChessNotation(localNotation.notation),
-      });
-    }
+      }, // close forward left
+      {
+        notation: {
+          x: direction * 1,
+          y: direction * -2,
+        },
+        type: MoveType.MOVE,
+      }, // close forward right
+
+      // backward moves
+      {
+        notation: {
+          x: direction * -1,
+          y: direction * 2,
+        },
+        type: MoveType.MOVE,
+      }, // close backward left
+      {
+        notation: {
+          x: direction * -1,
+          y: direction * -2,
+        },
+        type: MoveType.MOVE,
+      }, // close backward right
+      {
+        notation: {
+          x: direction * -2,
+          y: direction * 1,
+        },
+        type: MoveType.MOVE,
+      }, // far backward left
+      {
+        notation: {
+          x: direction * -2,
+          y: direction * -1,
+        },
+        type: MoveType.MOVE,
+      }, // far backward right
+    ];
 
     const absoluteMoves = moves.map((move) => this.sharedServive.localMoveToAbsoluteMove(square, move)).filter((move): move is IntermediaryMove => move !== null);
 
