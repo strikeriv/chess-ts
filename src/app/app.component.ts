a4bb0b8dff1ee398353f0155b2935455a4c2f10a
Matthew Craddock <matthew.j.craddock@live.com>
2025-10-20 15:36:46 -0500

    add tailwind back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BoardComponent } from "./features/board/board.component";
+import { BoardComponent } from './features/board/board.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, BoardComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [BoardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chess-ts';
