ce919061ee9a3a2cdfa4370fa43d7c14de275e24
Matthew Craddock <matthew.j.craddock@live.com>
2025-10-24 14:28:20 -0500

    missed one

--- src/app/features/board/piece/services/piece-moves/shared.service.ts
+++ src/app/features/board/piece/services/piece-moves/shared.service.ts
@@ -23,6 +23,8 @@ export class SharedService {
     if (this.isArrayNotationValid(absoluteNotation)) {
       return {
         notation: absoluteNotation,
+
+        origin: square,
         predecessor,
         type,
       };
